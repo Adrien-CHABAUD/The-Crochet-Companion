c4f6894686929feeb8d0d1c2c00b666e66eba342
Adrien CHABAUD <59933787+Adrien-CHABAUD@users.noreply.github.com>
2023-11-15 21:19:11 -0500

    Initial Commit

---
+++ The Crochet Companion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The Crochet Companion
+//
+//  Created by Adrien CHABAUD on 2023-11-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The Crochet Companion/The_Crochet_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Crochet_CompanionApp.swift
+//  The Crochet Companion
+//
+//  Created by Adrien CHABAUD on 2023-11-15.
+//
+
+import SwiftUI
+
+@main
+struct The_Crochet_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
